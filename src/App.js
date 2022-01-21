c96a4142c38b811f4301e2ac157473f0284adef1
Egecanyldrm <egecanyldrm@hotmail.com>
2022-01-21 22:24:52 +0300

    commit with vscode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+
+  // useEffect(() => {
+  //   fetch('https://restcountries.com/v2/all')
+  //     .then((response) => response.json())
+  //     .then((response) => { setCountries(response) })
+  // }, [])
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v2/all')
+      .then((response) => { setCountries(response.data) })
+      .catch((error) => { console.log(error); })
+  }, [])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container d-flex justify-content-center flex-wrap mt-5">
+      {countries.map((country) => {
+        return (
+          <div className='card col-2 m-2' key={country.name} >
+
+            <img className='card-img-top' src={country.flag} alt={country.name} style={{ height: 'auto' }} />
+            <div className="card-body">
+              <h5 className='card-title' >{country.name}</h5>
+              <p className='card-text' >{country.capital}</p>
+            </div>
+
+          </div>
+        )
+      })}
+
     </div>
   );
 }
